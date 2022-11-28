88bd46d8ba0bc3c55c027a186bd0ab61fb60fb79
xjkg <gripenja@arcada.fi>
2022-11-28 15:41:55 +0200

    Lektion 8, vidare gubbe, json

--- Lektion 7/jack.js
+++ Lektion 7/jack.js
@@ -2,10 +2,22 @@ function openjackBox(){
     document.querySelector("#jack-img > img").setAttribute('src', document.querySelector("#jack-img > img").getAttribute('data-open'));
 }
 
-document.querySelector('#jack-img').addEventListener('mousedown', openjackBox)
+document.querySelector('#jack-img > img').addEventListener('mousedown', openjackBox)
 
 function closejackBox(){
     document.querySelector("#jack-img > img").setAttribute('src', document.querySelector("#jack-img > img").getAttribute('data-closed'));
 }
 document.addEventListener('mouseup', closejackBox)
 
+const slider = document.querySelector("#slider");
+slider.addEventListener('input', () => 
+{
+    console.log(slider.value);
+    if(slider.value>70)
+    {
+        openjackBox()
+    }else{
+        closejackBox()
+    }
+
+})
